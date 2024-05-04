6f9027a3b4e4ad2b296cda9fc2baedbb7dcdb0db
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-05-04 15:57:59 +0530

    backend code

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+let posts = [
+    {
+        username : "Khuntapingu",
+        content : "Village Type"
+    },
+     {
+        username : "Binay",
+        content : "I Love Coding"
+     }
+];
+
 app.get("/", (req, res) =>{
     res.send("server is working");
 });
