0796a7a9e104785c9e0466620f60dfd3d2f77c2c
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-05-04 16:09:49 +0530

    backend code

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static(path.join(__dirname, "public")));
+
 let posts = [
     {
         username : "Khuntapingu",
