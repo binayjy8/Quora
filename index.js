9bf107372c65afdc0151640c485c474d539641ad
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-05-07 19:44:01 +0530

    backend code

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post("/posts", (req, res) => {
     res.redirect("/posts");
 });
 
-app.get("/posts/id:", (req, res) =>{
+app.get("/posts/:id", (req, res) =>{
     let { id } = req.params;
     let post = posts.find((p) => id === p.id);
     console.log(post);
