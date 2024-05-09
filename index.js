492b3d4c664b52744883e9703e695efe73a6aafb
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-05-09 15:29:01 +0530

    backend code

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.patch("/posts/:id", (req, res) => {
     let post = posts.find((p) => id === p.id);
     post.content = newContent;
     console.log(post);
-    res.send("patch request working");
+    res.redirect("/posts");
 });
 
 app.get("/posts/:id/edit", (req, res) => {
